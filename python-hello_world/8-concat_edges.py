81a1556bd0751e3783f08a2b5720963803d6ba74
kingsleykipkoech <k.kipkoech@alustudent.com>
2026-06-25 20:29:32 +0200

    all

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str = str[66:80] + str[113:122] + str[38:47] + str[:6]
+print(str)
