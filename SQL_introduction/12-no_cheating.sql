7c6e48dae4edef42079054d7d51676d804139c26
kingsleykipkoech <k.kipkoech@alustudent.com>
2026-07-30 20:04:50 +0200

    Add SQL Introduction project tasks 0-16

---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,2 @@
+-- Updates Bob's score to 10 using name field
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
---
+++ SQL_introduction/README.md
@@ -0,0 +1,3 @@
+# SQL - Introduction
+
+Introduction to SQL using MySQL 8.0 on Ubuntu 20.04 LTS.
