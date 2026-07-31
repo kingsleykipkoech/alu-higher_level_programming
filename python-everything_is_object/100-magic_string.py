702993f942d40405356a4984d181fa10f1c29aaa
kingsleykipkoech <k.kipkoech@alustudent.com>
2026-07-31 14:55:17 +0200

    Fix task 30 magic_string using mutable default argument

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    magic_string.strs = getattr(magic_string, 'strs', []) + ["BestSchool"]
-    return ", ".join(magic_string.strs)
+def magic_string(l=[]):
+    l.append("BestSchool")
+    return ", ".join(l)
